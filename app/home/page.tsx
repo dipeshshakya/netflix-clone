687d5b6b9712d9973a50233c642d6dc468ceadf2
Dipesh Shakya <dipeshshakya@Dipeshs-MacBook-Pro.local>
2023-12-10 09:59:47 +1100

    movie video banner component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import MovieVideo from '../components/MovieVideo'
 
 export default function Homepage () {
   return (
-    <div>autenticated users</div>
+    <div className="p-5 lg:p-0">
+    <MovieVideo />
+    <h1 className="text-3xl font-bold ">Recently Added</h1>
+  </div>
   )
 }
