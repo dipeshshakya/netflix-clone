90b2514bd70bcd0d19e9aa7aef7ae42be5857c25
Dipesh Shakya <dipeshshakya@Dipeshs-MacBook-Pro.local>
2023-12-09 12:32:08 +1100

    navbar component

---
+++ app/home/layout.tsx
@@ -0,0 +1,26 @@
+import { getServerSession } from "next-auth";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import Navbar from "../components/Navbar";
+import { authOptions } from "@/lib/auth";
+
+export default async function HomeLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/login");
+  }
+
+  return (
+    <>
+      <Navbar/>
+      <main className="w-full max-w-7xl mx-auto sm:px-6 lg:px-8">
+        {children}
+      </main>
+    </>
+  );
+}
