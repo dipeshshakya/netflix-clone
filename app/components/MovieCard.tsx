5f500975fa9bc03d8b2549568182dea627017be4
Dipesh Shakya <dipeshshakya@Dipeshs-MacBook-Pro.local>
2023-12-19 21:39:01 +1100

    watchlist component

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -1,30 +1,65 @@
-"use client"
+"use client";
 
-import { PlayCircle } from "lucide-react";
+import { Heart, PlayCircle } from "lucide-react";
 import PlayVideoModal from "./PlayVideoModal";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { addTowatchlist, deleteFromWatchlist } from "../action";
 
-interface iAppProps{
-    title: string;
-    overview: string;
-    movieId: number;
-    watchList: boolean;
-    wachtListId: number;
-    youtubeUrl: string;
-    year: number;
-    age: number;
-    time: number;
+interface iAppProps {
+  title: string;
+  overview: string;
+  movieId: number;
+  watchList: boolean;
+  wachtListId: string;
+  youtubeUrl: string;
+  year: number;
+  age: number;
+  time: number;
 }
 
+export function Moviecard({
+  movieId,
+  overview,
+  title,
+  youtubeUrl,
+  watchList,
+  wachtListId,
+  year,
+  age,
+  time,
+}: iAppProps) {
+  const [open, setOpen] = useState(false);
 
-export function Moviecard({movieId,overview,title,youtubeUrl, watchList ,wachtListId,year,age,time}:iAppProps){
-    const [open, setOpen] = useState(false);
+  const pathName = usePathname()
 
-    return (
-        <>
-           <button onClick={() => setOpen(true)} className="-mt-14">
+  return (
+    <>
+      <button onClick={() => setOpen(true)} className="-mt-14">
         <PlayCircle className="h-20 w-20" />
       </button>
+
+      <div className="right-5 top-5 absolute z-10">
+        {watchList ? (
+          <form action={deleteFromWatchlist}>
+            <input type="hidden" name="watchlistId" value={wachtListId} />
+            <input type="hidden" name="pathname" value={pathName} />
+            <Button variant="outline" size="icon">
+              <Heart className="w-4 h-4 text-red-500" />
+            </Button>
+          </form>
+        ) : (
+          <form action={addTowatchlist}>
+            <input type="hidden" name="movieId" value={movieId} />
+            <input type="hidden" name="pathname" value={pathName} />
+            <Button variant="outline" size="icon">
+              <Heart className="w-4 h-4" />
+            </Button>
+          </form>
+        )}
+      </div>
+
       <div className="p-5 absolute bottom-0 left-0">
         <h1 className="font-bold text-lg line-clamp-1">{title}</h1>
         <div className="flex gap-x-2 items-center">
@@ -49,6 +84,6 @@ export function Moviecard({movieId,overview,title,youtubeUrl, watchList ,wachtLi
         duration={time}
         release={year}
       />
-        </>
-    )
+    </>
+  );
 }
